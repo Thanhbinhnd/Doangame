6fd3c61088b6a6a9e17a3003bb1d79c0fec4f076
anhbinhdayyyy <129599941+Thanhbinhnd@users.noreply.github.com>
2023-05-20 15:11:15 +0700

    làm kại cuộc đời

---
+++ src/doangame/level.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package doangame;
+
+import java.util.logging.Level;
+
+/**
+ *
+ * @author binh0
+ */
+class level {
+
+    static Level SEVERE;
+    
+}
